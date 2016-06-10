bf5f6126243a16f80813c1d012e473b851a1a72c
sebastian.lastra <sebastian.lastra@cursor.cl>
2016-06-10 15:29:16 -0300

    add data; read files

--- script.py
+++ script.py
@@ -8,19 +8,22 @@
 import os
 import sys
 import argparse
+import json
 
 
 def main(arguments):
 
-    parser = argparse.ArgumentParser(description=__doc__,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('infile', help="Input file", type=argparse.FileType('r'))
-    parser.add_argument('-o', '--outfile', help="Output file",
-                        default=sys.stdout, type=argparse.FileType('w'))
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('infile_2015', help="Input file 2015", type=argparse.FileType('r'))
+    parser.add_argument('infile_2016', help="Input file 2016", type=argparse.FileType('r'))
+    parser.add_argument('-o', '--outfile', help="Output file", default=sys.stdout, type=argparse.FileType('w'))
 
     args = parser.parse_args(arguments)
+    data_2015 = json.load(args.infile_2015)
+    data_2016 = json.load(args.infile_2016)
 
-    print args
+    print len(data_2015)
+    print len(data_2016)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
